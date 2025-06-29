e4e805a52a1544fce92dd9e25d7f91e541c76b72
fadeldnswr <fadel.achmad@ui.ac.id>
2025-06-29 11:57:28 +0700

    create API boiler plate using FastAPI

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+Main script to run the application.
+'''
+
+from fastapi import FastAPI, HTTPException
+
+
+app = FastAPI(
+  title="Internet of Things and Digital Twin API",
+  description="API for creating simulation and prediction to Internet of Things data with Digital Twin concepts.",
+  version="0.1.0",
+  contact={
+    "name": "Fadel Achmad Daniswara",
+    "email": "daniswarafadel@gmail.com"
+  }
+)
+
+# Define a simple root endpoint
+@app.get("/")
+async def first_simple_route():
+  '''
+  Root endpoint to check if the API is running.
+  '''
+  return {
+    "message": "Welcome to the Internet of Things and Digital Twin API!",
+  }
