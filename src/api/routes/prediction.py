20aa23532c7774525488fdd37b74ed7008373fe5
fadeldnswr <fadel.achmad@ui.ac.id>
2025-07-02 09:44:33 +0700

    revised predicted API

--- src/api/routes/prediction.py
+++ src/api/routes/prediction.py
@@ -3,10 +3,11 @@ Prediction route for the IoT and Digital Twin application.
 This module defines the API endpoint for making predictions based on input data.
 '''
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from src.api.schema.input_prediction import InputPrediction
 from src.utils.utils import load_lstm_model
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 import numpy as np
 import pickle
@@ -33,8 +34,12 @@ with open(f"{FEATURE_TARGET_PATH}/real_target_scaler.pkl", "rb") as file:
 
 # Define manual prediction endpoint
 @router.post("/")
-async def predict(data:InputPrediction):
+async def predict(data:InputPrediction, hours: int = Query(default=1, description="Predict the data for the next 'n' hours")):
   try:
+    # Convert into minutes
+    steps = hours * 60
+    prediction = []
+    
     # Connvert input featurs into numpy array
     input_features = np.array([[
       data.temperature,
@@ -46,17 +51,34 @@ async def predict(data:InputPrediction):
       data.rolling_mean_rssi,
       data.rolling_std_rssi
     ]])
-    # Scale the input features
-    scaler = feature_scaler.transform(input_features)
-    # Reshape input features for the model
-    reshaped_features = scaler.reshape((1, 1, scaler.shape[1]))
-    # Scaled predictions
-    scaled_prediction = model.predict(reshaped_features)
-    # Inverse transform the scaled prediction to get the actual value
-    prediction = target_scaler.inverse_transform(scaled_prediction)
-    # Return the prediction as a float
-    return {
-      "prediction": float(prediction[0][0])
-    }
+    
+    # Create predictions for the specified number of steps
+    for _ in range(steps):
+      # Scale the input features
+      scaled_input = feature_scaler.transform(input_features)
+      
+      # Reshape the input for the LSTM model
+      reshaped_input = scaled_input.reshape((1, 1, scaled_input.shape[1]))
+      
+      # Scale the input for the model
+      scaled_output = model.predict(reshaped_input)
+      
+      # Inverse transform the scaled output to get the predicted value
+      predicted_value = target_scaler.inverse_transform(scaled_output)[0][0]
+      prediction.append(predicted_value) # Append the predicted value to the list
+      
+      # Update the input features for the next prediction
+      input_features[0][-3] = predicted_value  # Update the last feature (rssi) with the predicted value
+      
+      # Generate prediction time
+      timestamp = [datetime.now() + timedelta(minutes=i).strftime("%Y-%m-%dT%H:%M:%S") for i in range(steps)]
+      
+      # Return the prediction results
+      return {
+        "data": prediction,
+        "steps": steps,
+        "timestamp": timestamp,
+        "hours_predicted": hours
+      }
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
